60b37042da1d9722a5b440b148e44678379350c5
SU3K5T <surikat0013@icloud.com>
2023-07-20 17:44:16 +0300

    modified foobar.rb

--- lection-2/foobar.rb
+++ lection-2/foobar.rb
@@ -1,6 +1,6 @@
 def foobar(firstNum, secondNum) 
-    return (firstNum == 20 || secondNum == 20) ? secondNum : firstNum + secondNum
+    return (firstNum == 20 || secondNum == 20) ? secondNum : firstNum.to_i + secondNum.to_i
 end 
 
-puts foobar(10, 4)
+puts foobar("10", 4)
 puts foobar(20, 30)
